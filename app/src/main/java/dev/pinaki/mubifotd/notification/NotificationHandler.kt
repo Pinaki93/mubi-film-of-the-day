3952e1ea340efe726c1efb7e681f782b4a01dddd
pinaki93 <pinakiacharyagd@gmail.com>
2022-12-04 17:43:36 +0530

    add click intent to notification

--- app/src/main/java/dev/pinaki/mubifotd/notification/NotificationHandler.kt
+++ app/src/main/java/dev/pinaki/mubifotd/notification/NotificationHandler.kt
@@ -1,8 +1,12 @@
 package dev.pinaki.mubifotd.notification
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import dev.pinaki.mubifotd.MainActivity
 import dev.pinaki.mubifotd.R
 import dev.pinaki.mubifotd.domain.FilmOfTheDay
 
@@ -22,11 +26,31 @@ class RealNotificationHandler(
         if (filmOfTheDay.year > 0) {
             contentTextBuilder.append(" (${filmOfTheDay.year})")
         }
+        if (filmOfTheDay.directors.isNotEmpty()) {
+            contentTextBuilder.append(" by ${filmOfTheDay.directors.joinToString()}")
+        }
+
+        val appOpenIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        } else {
+            PendingIntent.FLAG_UPDATE_CURRENT
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            appOpenIntent,
+            flags
+        )
+
         val notification = NotificationCompat.Builder(context, filmOfTheDayChannel.id)
             .setSmallIcon(R.mipmap.ic_launcher)
             .setContentTitle(context.getString(R.string.mubi_film_of_the_day))
             .setContentText(contentTextBuilder.toString())
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
 
         NotificationManagerCompat.from(context)
